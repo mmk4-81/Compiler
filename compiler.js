c919d02a73674b6789ec6af8f3c3de09168ef5a3
mmk <mmkkarbalaei@gmail.com>
2024-02-15 15:11:33 +0330

    test output

---
+++ compiler.js
@@ -0,0 +1,5 @@
+function compileCode() {
+    var inputCode = document.getElementById("input-code").value;
+    var compiledCode = "Compiled code will be here...";
+    document.getElementById("output").innerHTML = compiledCode;
+}
